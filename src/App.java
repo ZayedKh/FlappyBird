9c1100d88f65b481c586f9356f629958581ee1d5
Zayed <zayedkh02@gmail.com>
2025-03-16 19:48:48 +0000

    Frame addtions, made flappybird class

--- src/App.java
+++ src/App.java
@@ -3,13 +3,16 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) {
-        // board dimensions
+        // board dimensions - dimensions of background image
         int boardWidth = 360;
         int boardHeight = 640;
 
         // create new frame, title it, and place in center of screen
         JFrame frame = new JFrame("Flappy Bird");
         frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
