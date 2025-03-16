e13f2b515c3990066a5a81791208464372291e34
Zayed <zayedkh02@gmail.com>
2025-03-16 19:57:26 +0000

    Drew background image onto panel

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -17,13 +17,22 @@ public class FlappyBird extends JPanel {
 
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
-        setBackground(Color.blue);
 
         //load images
         backgroundImage = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
         birdImage = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
         topPipeImage = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
         bottomPipeImage = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        // draw background image
+        g.drawImage(backgroundImage, 0, 0, boardWidth, boardHeight, null);
     }
 
 }
